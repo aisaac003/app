3f70285c4020840f0bc091d41a2dc20423a634db
aisaac003 <122706463+aisaac003@users.noreply.github.com>
2023-01-30 00:00:53 -0500

    Delete op3.py

--- op3.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import cv2 as cv
-import open3d as o3d 
-
-carpeta = "D:\Ingeniería Mecánica Eléctrica\Ciclo VII - 2023\Sistemas Automáticos y Control (SAC)\Proyecto_semestral\Programación\app\fragment.ply"
-
-pcd = o3d.visualization.draw_geometries(carpeta)
-print (pcd)
